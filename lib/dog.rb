9a6d9d2abe9ca33b785b226aa24c9dd8f1d96b6b
michaelstsnow <michael.st.snow@gmail.com>
2019-06-15 02:18:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,16 +61,16 @@ class Dog
     WHERE id=?
     SQL
     dog_row=DB[:conn].execute(sql,self.id)
-    
+
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
   end
-  
+
   def find_or_create_by
   end
 
-  
+
 
   def self.find_by_name
   end
