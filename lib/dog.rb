330995817b083b341459388f6d4cb71b6213204e
michaelstsnow <michael.st.snow@gmail.com>
2019-06-15 02:13:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,22 +2,56 @@ class Dog
 
   attr_accessor :id, :name, :breed
 
-  def initialize(name:,breed:,id:=nil)
-    @name=:name
-    @breed=:breed
-    @id=:id
+  def initialize( name:, breed:, id:nil)
+    @name=name
+    @breed=breed
+    @id=id
   end
 
   def self.create_table
+    sql=<<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
+    sql=<<-SQL
+    DROP TABLE IF EXISTS dogs
+    SQL
+    DB[:conn].execute(sql)
   end
 
   def save
+    if self.id
+      self.update
+    else
+      sql=<<-SQL
+      INSERT INTO dogs (name,breed)
+      VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql,self.name,self.breed)
+      @id=DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
   end
 
-  def self.create
+  def update
+    sql=<<-SQL
+    UPDATE dogs
+    SET name = ?, breed = ?
+    WHERE id=?
+    SQL
+    DB[:conn].execute(sql,self.name,self.breed,self.id)
+  end
+
+  def self.create(hash)
+    dog=Dog.new(name: hash[:name], breed: hash[:breed])
+    dog.save
   end
 
   def self.find_by_id
@@ -32,6 +66,5 @@ class Dog
   def self.find_by_name
   end
 
-  def update
-  end
+
 end
